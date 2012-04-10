2162fd3ea53424794f910e8ffad3f12d17e7a392
Kyongjin Seo <gkhackers@gmail.com>
2012-04-10 16:34:31 +0900

    bundle install

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 require "bundler/capistrano"
 
-server "211.42.140.111", :web, :app, :db, primary: true
+server "211.42.140.111", :web, :app, :db, :primary => true
 
 set :application, "blog"
 set :user, "deployer"
@@ -20,12 +20,12 @@ after "deploy", "deploy:cleanup" # keep only the last 5 releases
 namespace :deploy do
   %w[start stop restart].each do |command|
     desc "#{command} unicorn server"
-    task command, roles: :app, except: {no_release: true} do
+    task command, :roles => :app, :except => {:no_release => true} do
       run "/etc/init.d/unicorn_#{application} #{command}"
     end
   end
 
-  task :setup_config, roles: :app do
+  task :setup_config, :roles => :app do
     sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
     sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
     run "mkdir -p #{shared_path}/config"
@@ -34,13 +34,13 @@ namespace :deploy do
   end
   after "deploy:setup", "deploy:setup_config"
 
-  task :symlink_config, roles: :app do
+  task :symlink_config, :roles => :app do
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
   end
   after "deploy:finalize_update", "deploy:symlink_config"
 
   desc "Make sure local git is in sync with remote."
-  task :check_revision, roles: :web do
+  task :check_revision, :roles => :web do
     unless `git rev-parse HEAD` == `git rev-parse origin/master`
       puts "WARNING: HEAD is not the same as origin/master"
       puts "Run `git push` to sync changes."
