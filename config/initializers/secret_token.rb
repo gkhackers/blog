15b8905e3e9e8e893aef013409fdfac0d8e7d880
Kyongjin Seo <gkhackers@gmail.com>
2012-04-10 13:29:35 +0900

    Deploying config files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '8c1f88fc29e472802e85e544e0c593fc908145eaee3bb918cd8e2844506371b2a6aee750f6289cd20b99bd2f89270b725d64a2796c19d46374dd595d388a0adc'
